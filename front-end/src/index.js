4a3c57fb8a741877f3199161cf158393b654dfc8
Robert Komaromi <chipit24@users.noreply.github.com>
2021-04-26 19:57:28 -0400

    fix icon fonts

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,7 @@ import moment from 'moment-timezone';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './normalize.css';
-import 'material-icons/css/material-icons.min.css';
+import 'material-design-icons-iconfont';
 import './config/global-styles';
 import store from '@/store';
 import axiosInit from './config/axios_config';
